6eed0e2ff15ac5d457ac98b6a32e56625a6bef7b
Lautaro261 <nahuelygoofy@gmail.com>
2023-03-20 20:06:02 -0300

    Parte 1 lista

--- 02-WebServer/homework/01 - Exercises/server.js
+++ 02-WebServer/homework/01 - Exercises/server.js
@@ -9,7 +9,7 @@ module.exports =
   /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
  http.createServer((request, response)=>{
   console.log(`Server raised in port ${PORT}`);
-  if(request.url ==="/api"){
+  /* if(request.url ==="/api"){
     fs.readFile("../01 - Exercises/utils/dogsData.json", (err,data)=>{
       if(err){
         response.writeHead(404, { "Content-Type" : "text-plain"});
@@ -19,8 +19,42 @@ module.exports =
         response.end(data)
       }
     })
-    return
   }
+    return */
+    const { url } = request;
+    switch(url ){
+
+      case "/api":
+        fs.readFile("./utils/dogsData.json", (err,data)=>{
+          if(err){
+            response.writeHead(404, { "Content-Type" : "text-plain"})
+            response.end("json not found")
+          }else{
+            response.writeHead(200, { "Content-Type" :"application/json"})
+            response.end(data)
+          }
+        })
+        break; 
+
+      case "/allDogs":
+        fs.readFile("./utils/allDogs.html", "utf-8", (err,data)=>{
+          if(err){
+            response.writeHead(404, { "Content-Type" : "text-plain" })
+            response.end("html not found");
+          }else{
+            response.writeHead(200, { "Content-Type": "text/html" })
+            response.end(data)
+          }
+        })
+        break;
+
+      default:
+        response.writeHead(404, { "Content-Type" : "text-plain"} )
+        response.end("Route not found")
+        break;
+    }
+
+  
 
  })
  .listen(PORT, "localhost")
